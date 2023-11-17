3942fbe2106f6f69df0428b08e2345bbefe81530
leezhuuu <69389053+leezhuuu@users.noreply.github.com>
2023-11-17 23:08:35 +0800

    Update config.py

--- lms/config.py
+++ lms/config.py
@@ -1,18 +1,18 @@
 api = {
     "xinghuo": {
-        "appid": "eb36a74a",    #填写控制台中获取的 APPID 信息
-        "api_secret": "M2IyOWJhOWIzZTYwMjAyYjc5M2E5OTlj",   #填写控制台中获取的 APISecret 信息
-        "api_key": "16c32f307e97db26e2038f43a9dd8cc9",  #填写控制台中获取的 APIKey 信息
-        "domain": "general",    #用于配置大模型版本，默认“general/generalv2”
-        "Spark_url": "ws://spark-api.xf-yun.com/v1.1/chat"  # v1.5环境的地址
+        "appid": "xxxxxxxx",    #填写控制台中获取的 APPID 信息
+        "api_secret": "xxxxxxxx",   #填写控制台中获取的 APISecret 信息
+        "api_key": "xxxxxxxx",  #填写控制台中获取的 APIKey 信息
+        "domain": "xxxxxxxx",    #用于配置大模型版本，默认“general/generalv2”
+        "Spark_url": "xxxxxxxx"  # v1.5环境的地址
     },
     
     "ChatGPT": {
-        "apikey": "",
-        "model": "",
+        "apikey": "xxxxxxxx",
+        "model": "xxxxxxxx",
         "apiurl": "https://api.openai.com/chat/completions"
     },
-
+#本地模型
     "RWKV": {
         "apikey": "114514",
         "model": "rwkv",
